a989f7a78bf93b9bee7f4569980b73cf298633b9
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-19 14:42:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+// // let cats = ["Milo", "Otis", "Garfield"];
+
+// // function nondestructivelyAppendCat(name) {
+// //   return [...cats, name];
+// // }
+
+// // let newCats = nondestructivelyAppendCat("Ralph");
+// // console.log(cats);
+// // console.log(newCats);
+
+// // let cats = ["Milo", "Otis", "Garfield"];
+
+// // function nondestructivelyRemoveLastCat() {
+// //   return cats.slice(0, -1);
+// // }
+
+// let newCats = nondestructivelyRemoveLastCat();
+// console.log(newCats); // output: ["Milo", "Otis"]
+// console.log(cats); // output: ["Milo", "Otis", "Garfield"]
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function nondestructivelyRemoveFirstCat() {
+  return cats.slice(1);
+}
+
+let newCats = nondestructivelyRemoveFirstCat();
+console.log(newCats); // output: ["Otis", "Garfield"]
+console.log(cats); // output: ["Milo", "Otis", "Garfield"]
+
